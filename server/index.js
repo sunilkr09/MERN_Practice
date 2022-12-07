cf637458e405005c20a2b441271b37dba99f2133
Sunil Kumar Mallah <sk2321996@gmail.com>
2022-12-07 11:58:26 +0530

    Added Post Routes and One time Mock Data

--- server/index.js
+++ server/index.js
@@ -10,7 +10,13 @@ import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
-import { register } from "./controllers/auth";
+import postRoutes from "./routes/posts.js";
+import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
 /* configuration */
 const __filename = fileURLToPath(import.meta.url);
@@ -39,10 +45,12 @@ const upload = multer({ storage });
 
 /* Routes with files */
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* Routes */
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 /* mongoose setup */
 const PORT = process.env.PORT || 6001;
@@ -51,4 +59,8 @@ mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true
 }).then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    // /* Add data one time */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
 }).catch((error) => console.log(`${error} did not connect`));
